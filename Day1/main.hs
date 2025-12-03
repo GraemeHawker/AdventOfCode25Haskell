55a7f777c74dbd47ee0288c7442fc2f30079b778
Graeme Hawker <graeme@lutralutra.co.uk>
2025-12-03 03:33:05 +0000

    cleaned up template

--- Day1/main.hs
+++ Day1/main.hs
@@ -1,12 +1,6 @@
 import Control.Arrow ((&&&))
 
-import Data.List (sort)
-import Data.Map.Strict (findWithDefault, fromListWith, assocs)
-
-type Input = [(Int, Int)]
-
-each :: (a -> b) -> (a, a) -> (b, b)
-each f (a, b) = (f a, f b)
+type Input = [Int]
 
 part1 :: Input -> Int
 part1 = _ 
@@ -15,9 +9,7 @@ part2 :: Input -> Int
 part2 = _
 
 prepare :: String -> Input
-prepare = map (line . words) . lines
-  where line [x, y] = (read x, read y)
-        line invalid = error (show invalid)
+prepare = _
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
